c687a387e2ee26404f11b696237752cd29a027a7
Scott Greenwood <greenwoodms@ornl.gov>
2019-05-03 13:52:09 -0400

    rename example to avoid conflicts

--- TRANSFORM/Resources/Scripts/jModelica/Math/Examples/check_psi_math.py
+++ TRANSFORM/Resources/Scripts/jModelica/Math/Examples/check_psi_math.py
@@ -2,7 +2,7 @@ from pymodelica import compile_fmu
 from pyfmi import load_fmu
 
 libPath = r'C:\Users\vmg\Documents\Modelica\TRANSFORM-Library/TRANSFORM'
-modelName = 'TRANSFORM.Math.Examples.check_psi'
+modelName = 'TRANSFORM.Math.Examples.check_psi_math'
 
 fmu = compile_fmu(modelName,libPath,target='cs')
 model = load_fmu(fmu)
