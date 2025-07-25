5f0343eb92aac40638364ec1b9ebdd042efade55
Scott Greenwood <greenwoodms@ornl.gov>
2025-07-25 16:56:03 -0400

    removing reference results from transform-library (e.g., to avoid path length issues with submodules) and moving to their own repo

--- regtestsLinux.py
+++
@@ -1,8 +0,0 @@
-import os
-import buildingspy.development.regressiontest as r
-rt = r.Tester(check_html=False)
-myMoLib = os.path.join("TRANSFORM")
-rt.setLibraryRoot(myMoLib)
-#rt.showGUI(False)
-rt.setNumberOfThreads(2)
-rt.run() 
